ce048c82739c52aec6b5641e399cad8f1f6d989f
hieu hoang <hieu.h@cs.ventures>
2021-12-18 12:01:31 +0700

    setup devise and migrate data

---
+++ spec/factories/users.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  first_name             :string(255)      not null
+#  last_name              :string(255)      not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#
+FactoryBot.define do
+  factory :user do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    email { "MyString" }
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  first_name             :string(255)      not null
+#  last_name              :string(255)      not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
