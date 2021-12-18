a30e7f53538ced9fddcd689da09d8936b279058a
hieu hoang <hieu.h@cs.ventures>
2021-12-18 13:57:36 +0700

    apply video embed generator url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+
+  root to: 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
